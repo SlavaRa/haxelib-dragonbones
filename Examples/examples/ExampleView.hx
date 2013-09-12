5ce90ce305f2ed0366a42a73c8dd701fa46d3879
slavara <ratdg@ya.ru>
2013-09-12 11:12:47 +0400

    --minor

--- Examples/examples/ExampleView.hx
+++ Examples/examples/ExampleView.hx
@@ -7,8 +7,6 @@ package examples;
 typedef ExampleView = examples.starling.StarlingView;
 #elseif gm2d
 typedef ExampleView = examples.gm2d.GM2DView;
-#elseif flixel
-typedef ExampleView = examples.flixel.FlixelView;
 #else
 typedef ExampleView = examples.openfl.OpenFLView;
 #end
